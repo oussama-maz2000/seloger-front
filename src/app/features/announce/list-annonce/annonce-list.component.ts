43d95120b61e9c47d29352facb5ddafd38724bc0
oussama-maz2000 <loulououssama2015@gmail.com>
2023-11-05 15:45:00 +0100

    ...

--- src/app/features/announce/list-annonce/annonce-list.component.ts
+++ src/app/features/announce/list-annonce/annonce-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { willaya } from 'src/app/core/shared/data';
 import { getAnnounces } from 'src/app/store/selector/announce.selector';
@@ -10,12 +11,25 @@ import { getAnnounces } from 'src/app/store/selector/announce.selector';
 })
 export class AnnonceListComponent implements OnInit {
   willays: string[];
+  besoinType = ['House', 'appartement', 'Terrain', 'Local commercial'];
 
-  constructor(private store: Store) {
+  selectedBesoin = [];
+  filterForm: FormGroup;
+  constructor(private store: Store, private formBuilder: FormBuilder) {
     this.willays = willaya;
+    this.filterForm = this.formBuilder.group({
+      type: ['louer'],
+      willaya: ['Batna'],
+      budget: [2000],
+      besoin: [['House', 'appartement']],
+    });
   }
 
   ngOnInit(): void {
     this.store.select(getAnnounces).subscribe(console.log);
+  }
+
+  onSubmit() {
+    console.log(this.filterForm.value);
   }
 }
