ddc81f5d633e7654994e5e9bba78168ddb73f4d7
oussama-maz2000 <loulououssama2015@gmail.com>
2023-11-12 14:45:37 +0100

    ...

--- src/app/features/test/test/test.component.ts
+++ src/app/features/test/test/test.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { willaya } from 'src/app/core/shared/data';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css']
 })
 export class TestComponent {
+  willaya:string[]
+  constructor() {
+    this.willaya = willaya;
+   }
 
 }
