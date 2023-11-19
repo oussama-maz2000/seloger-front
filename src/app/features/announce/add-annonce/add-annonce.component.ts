a5b25999b070b7d570274c2a7338c576bedef15a
oussama-maz2000 <loulououssama2015@gmail.com>
2023-11-19 15:28:17 +0100

    ...

--- src/app/features/announce/add-annonce/add-annonce.component.ts
+++ src/app/features/announce/add-annonce/add-annonce.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-annonce',
   templateUrl: './add-annonce.component.html',
-  styleUrls: ['./add-annonce.component.css']
+  styleUrls: ['./add-annonce.component.css'],
 })
 export class AddAnnonceComponent {
+  files: File[] = [];
 
+  onSelect(event: any): void {
+    /* if (event.addedFiles[0].size < 2222222222) {
+      this.files.push(...event.addedFiles);
+    } */
+    /*  const imageArray = this.fromRequired.get('images') as FormArray; */
+    this.files.push(...event.addedFiles);
+    for (const file of this.files) {
+      /*  imageArray.push(new FormControl(file)); */
+    }
+  }
+
+  onRemove(event) {
+    console.log(event);
+    this.files.splice(this.files.indexOf(event), 1);
+  }
 }
